6b52961dd1a36ad2431946224604b0d1875c0271
agasenka <agasptama@gmail.com>
2026-06-29 18:55:14 +0700

    add libx11

--- modules/system/nix-ld.nix
+++ modules/system/nix-ld.nix
@@ -11,6 +11,33 @@
       openssl
       stdenv.cc.cc
       zlib
+
+      xorg.libX11
+      xorg.libXext
+      xorg.libXi
+      xorg.libXrender
+      xorg.libXtst
+      xorg.libXrandr
+      xorg.libXcursor
+      xorg.libXfixes
+      xorg.libXinerama
+      xorg.libxcb
+
+      glib
+      gtk3
+      cairo
+      pango
+      gdk-pixbuf
+      atk
+
+      libGL
+      mesa
+
+      alsa-lib
+      dbus
+      expat
+      fontconfig
+      freetype
     ];
   };
 }
