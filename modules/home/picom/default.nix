eadf6b2c1fc6f2c639b08e87dcad4ae0a604c22a
agasenka <agasptama@gmail.com>
2026-07-11 19:27:19 +0700

    fix(picom): make fullscreen windows fully opaque

--- modules/home/picom/default.nix
+++ modules/home/picom/default.nix
@@ -11,29 +11,36 @@
 
     fade = true;
     fadeDelta = 10;
-    fadeSteps = [
-      0.03
-      0.03
-    ];
-    shadow = true;
+    fadeSteps = [ 0.03 0.03 ];
 
+    shadow = true;
+    shadowOffsets = [ (-7) (-7) ];
     shadowExclude = [
       "name = 'Notification'"
       "class_g = 'Conky'"
       "class_g ?= 'Notify-osd'"
       "class_g = 'Cairo-clock'"
       "_GTK_FRAME_EXTENTS@:c"
+      "_NET_WM_STATE@:32a *= '_NET_WM_STATE_FULLSCREEN'"
+      "window_type = 'fullscreen'"
+    ];
+
+    opacityRules = [
+      "100:fullscreen"
+      "100:_NET_WM_STATE@:32a *= '_NET_WM_STATE_FULLSCREEN'"
+      "100:window_type = 'fullscreen'"
+      "100:name *?= 'YouTube'"
     ];
 
     settings = {
-      shadow-radius = 7;
-      shadow-offset-x = -7;
-      shadow-offset-y = -7;
-
+      unredir-if-possible = true;
       corner-radius = 7;
+
       rounded-corners-exclude = [
         "window_type = 'dock'"
         "window_type = 'desktop'"
+        "_NET_WM_STATE@:32a *= '_NET_WM_STATE_FULLSCREEN'"
+        "window_type = 'fullscreen'"
       ];
 
       blur = {
@@ -41,17 +48,11 @@
         strength = 7;
         kern = "3x3box";
       };
-      blur-background = true;
-      blur-background-exclude = [ ];
 
-      opacity-rule = [
-        "100:_NET_WM_STATE@:32a *= '_NET_WM_STATE_FULLSCREEN'"
-        "100:window_type = 'fullscreen'"
-        "100:name *?= 'YouTube'"
-        "100:name *?= 'Netflix'"
-        "100:name *?= 'Prime Video'"
-        "100:class_g = 'firefox' && _NET_WM_STATE@:32a *= '_NET_WM_STATE_FULLSCREEN'"
-        "100:class_g = 'Chromium' && _NET_WM_STATE@:32a *= '_NET_WM_STATE_FULLSCREEN'"
+      blur-background = true;
+      blur-background-exclude = [
+        "_NET_WM_STATE@:32a *= '_NET_WM_STATE_FULLSCREEN'"
+        "window_type = 'fullscreen'"
       ];
     };
   };
